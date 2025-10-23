9c564fb4ae1bc003b4317b6f4f7d205e4429d28f
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-10-23 10:56:16 -0500

    Bootstrapping the Host pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
 import Layout from "./components/Layout";
 import "./App.css";
 
@@ -16,6 +19,9 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
           <Route path="/about" element={<About />} />

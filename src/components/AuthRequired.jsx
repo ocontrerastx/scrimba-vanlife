06e237b9e222cd5ad6af72525d9148d44fefa795
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-10-30 15:33:01 -0500

    Updated app to include logic for handling dummy auth / Login pages

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 export default function AuthRequired() {
-  const authenticated = false;
+  const authenticated = true;
 
   if (!authenticated) {
-    return <Navigate to="/login" />;
+    return (
+      <Navigate to="/login" state={{ message: "You must log in first" }} />
+    );
   }
   return <Outlet />;
 }

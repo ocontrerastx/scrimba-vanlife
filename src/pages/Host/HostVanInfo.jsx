45f211ae789270cce57a2433db05a6aab924f656
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-10-31 13:33:58 -0500

    Revert "Updated api to use firebase rather than mirage JS and cleaned up CSS on van detail page"
    This reverts commit ab0cca3725e4576c5dd495eada6c9085c982577f.

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useOutletContext } from "react-router-dom";
 
 export default function HostVanInfo() {
-  const { currentVan } = useOutletContext();
+  const currentVan = useOutletContext();
   return (
     <section className="host-van-detail-info">
       <h4>
